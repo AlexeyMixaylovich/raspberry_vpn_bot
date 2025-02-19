884f9b1cbb9f4a59cbe7c0d25e778877f6decf7a
aleksey.zubkov <aleksey.zubkov@sravni.ru>
2025-02-19 14:15:14 +0300

    fix

--- src/helpers/getOpenVpnConfig.ts
+++ src/helpers/getOpenVpnConfig.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { execPromise } from './execPromise';
 
-export const getConfigLines = async (): Promise<string[] | undefined> => {
+const getConfigLines = async (): Promise<string[] | undefined> => {
   const res = await execPromise('ps aux | grep [o]penvpn');
   if (res.status === 'success') {
     return res.data.split('\n');
@@ -9,12 +9,8 @@ export const getConfigLines = async (): Promise<string[] | undefined> => {
   return undefined;
 };
 
-const getConfigLinesMock = (): Promise<string[] | undefined> => Promise.resolve([
-  'root         750  0.0  0.4  10668  3848 ?        Ss   15:57   0:00 /usr/bin/sudo /usr/sbin/openvpn --config /home/alexey/opnen_vpn_config/Germany, Limburg S9.ovpn',
-  'root         784  2.3  0.9  13700  8832 ?        S    15:57   4:23 /usr/sbin/openvpn --config /home/alexey/opnen_vpn_config/Germany, Limburg S9.ovpn',
-]);
 export async function getOpenVPNConfig(): Promise<string | undefined> {
-  const lines = await getConfigLinesMock();
+  const lines = await getConfigLines();
   if (!lines) {
     return undefined;
   }
